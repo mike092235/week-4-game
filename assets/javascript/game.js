e5803f75c69bf46927853a57fe17b1ae693c4db6
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-12-01 16:39:56 -0500

    crystalgame

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//GLOBAL VARIABLES
+//=================================================================================================
+var wins = 0; // numbers of wins
+var losses = 0; // numbers of losses
+var currentScore = 0; // player's current score
+var targetScore = 0; // targer score
+var diamondVal = 0; // value of diamond
+var pinkGemVal = 0; // value of pink gem
+var rubyGemVal = 0; // value of ruby gem
+var whiteGemVal = 0; // value of white gem
+
+
+
+//FUNCTIONS (Reusable blocks of code that I will call upon when needed)
+//=================================================================================================
+$(document).ready(function(){
+  
+})
+
+
+
+//MAIN PROCESS
+//=================================================================================================
