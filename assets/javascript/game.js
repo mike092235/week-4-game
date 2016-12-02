2081fa27114a9b0247549cd8b856605f2c892222
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-12-02 14:08:38 -0500

    crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,13 @@
 var wins = 0; // numbers of wins
 var losses = 0; // numbers of losses
 var currentScore = 0; // player's current score
-var targetScore = 0; // targer score
+var targetScore = 0; // target score
 var diamondVal = 0; // value of diamond
 var pinkGemVal = 0; // value of pink gem
 var rubyGemVal = 0; // value of ruby gem
 var whiteGemVal = 0; // value of white gem
 
 
-
-//FUNCTIONS (Reusable blocks of code that I will call upon when needed)
-//=================================================================================================
 $(document).ready(function(){
   var currentScore = 0;
   var targetScore = Math.floor(Math.random()*101 + 19);
@@ -20,10 +17,10 @@ $(document).ready(function(){
   var pinkGemVal = Math.floor(Math.random()*12 + 1);
   var rubyGemVal = Math.floor(Math.random()*12 + 1);
   var whiteGemVal = Math.floor(Math.random()*12 + 1);
-  $
-})
+  $('#targetScore').html(targetScore);
+  $('#diamond').attr("data-num", diamondVal);
+  $('#pink-gem').attr("data-num", pinkGemVal);
+  $('#ruby').attr("data-num", rubyGemVal);
+  $('#white-gem').attr("data-num", whiteGemVal);
+});
 
-
-
-//MAIN PROCESS
-//=================================================================================================
